52231a39b2d2ae6b3bf4344ea5e6372445a61b3d
Cuque <ecuqueo1@miumg.edu.gt>
2024-04-21 13:51:12 -0600

    cambio de titulo en login_page de Login Screens a Login

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -11,7 +11,7 @@ class login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Screen',
+      title: 'Login',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
